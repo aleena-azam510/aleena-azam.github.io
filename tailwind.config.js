1655dba1aa72efd0a1c49f7837625e6d8dc489e0
aleena-azam510 <aleenaazamchandio12@gmail.com>
2025-09-07 09:14:58 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}", // This is a good general-purpose path
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
